9a93655ebaa3c5b18092ae805bfb9b8fe9efa28b
Adnan <kidwai.a@gmail.com>
2019-06-07 01:36:08 -0400

    Adding .gitignore file

---
+++ p3/p3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  p3
+//
+//  Created by Adnan Kidwai on 6/7/19.
+//  Copyright © 2019 Adnan Kidwai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
